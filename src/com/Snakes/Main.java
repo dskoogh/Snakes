463b857f8dde72ac7250cbc079c36508f3a0462e
Dafvid Skoogh <dafvid.skoogh@gmail.com>
2017-10-17 09:15:27 +0200

    Added update

--- src/com/Snakes/Main.java
+++ src/com/Snakes/Main.java
@@ -1,8 +1,19 @@
 package com.Snakes;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        // Menu, deciding how many players/AIs to participate
+
+        while(true) {
+            // Game starting
+
+            do {
+                // do / while loop tickar framåt.
+                // Movementklass som översätter key till ett riktningsbyte
+
+            } while (key != null);
+        // Playerklass - längd, kollisionsavkänning
+        // Utskrivningsklass
+        }
     }
 }
