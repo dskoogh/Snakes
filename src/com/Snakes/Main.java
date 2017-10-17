152e9d8b8031af7aa04cfcc290b04313b0953047
Kemal Demirtas <demirtas@kth.se>
2017-10-17 11:35:07 +0200

    Changes -kemal

--- src/com/Snakes/Main.java
+++ src/com/Snakes/Main.java
@@ -4,15 +4,14 @@ import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.nio.charset.Charset;
+import com.googlecode.lanterna.input.Key;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
-        terminal.enterPrivateMode();
-        terminal.setCursorVisible(false);
-
+    public static void main(String[] args) throws InterruptedException {
+        Terminal terminal = getTerminal();
         Player playerOne = new Player();
-
+        Key key = terminal.readInput();
         
         // Menu, deciding how many players/AIs to participate. Level
 
@@ -22,15 +21,19 @@ public class Main {
         while(true) {
 
             terminal.clearScreen();
+            // Put player on terminal
+            terminal.moveCursor(playerOne.getHead().getX(), playerOne.getHead().getY());
+            terminal.putCharacter('O');
+            
+            // Sleep
+            Thread.sleep(500);
+            
+            
+            playerOne.keyListen(key);
+            playerOne.move();
+            
 
-            terminal.moveCursor(playerOne.getTail(playerOne.getTail().size()));
-
-            //skriv ut
-            //sov
-            //key
-
-            // Game starting
-
+            
             // Keylyssnarklass som avslyssnar vilka keys som trycks
 
             // Movementklass som översätter key till ett riktningsbyte - en per spelare
@@ -39,5 +42,12 @@ public class Main {
 
             // Väntar X länge
         }
+    }
+    
+    private static Terminal getTerminal() {
+        Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+        terminal.setCursorVisible(false);
+        return terminal;
     }
 }
--- src/com/Snakes/Player.java
+++ src/com/Snakes/Player.java
@@ -1,24 +1,75 @@
 package com.Snakes;
 
+import com.googlecode.lanterna.input.Key;
+
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 import java.util.Random;
 
+
 public class Player {
-
-    private Queue<Tail> tail = new LinkedList<>();
+    
+    private List<Tail> tail = new LinkedList<>();
     private boolean up, down, right, left;
-
+    private char dir;
+    final char UP = 'w', DOWN = 's', LEFT = 'a', RIGHT = 'D';
+    
     public Player() {
         Random random = new Random();
-
+        
         this.tail.add(new Tail(random.nextInt(20), random.nextInt(20)));
         this.right = true;
-
     }
-
-    public Queue<Tail> getTail() {
-        return tail;
+    
+    public Tail getHead() {
+        return tail.get(tail.size() - 1);
+    }
+    
+    public void keyListen(Key key) {
+        // Listen to where to go next
+        dir = key.getCharacter();
+    }
+    
+    
+    public void move() {
+        // Update direction
+        switch (dir) {
+            case UP:
+                if (!up && !down) {
+                    up = true;
+                    down = false;
+                    left = false;
+                    right = false;
+                }
+                break;
+            case DOWN:
+                if (!up && !down) {
+                    up = false;
+                    down = true;
+                    left = false;
+                    right = false;
+                }
+                break;
+            case RIGHT:
+                if (!right && !left) {
+                    up = false;
+                    down = false;
+                    left = false;
+                    right = true;
+                }
+                break;
+            case LEFT:
+                if (!right && !left) {
+                    up = false;
+                    down = false;
+                    left = true;
+                    right = false;
+                }
+                break;
+        }
+        
+        // Add new Tail object in list tail, in direction bool
+        
     }
 }
 
