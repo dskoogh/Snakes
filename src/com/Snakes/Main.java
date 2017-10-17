de0eba91dfcc7c2600724351056fc84c2af4145c
Dafvid Skoogh <dafvid.skoogh@gmail.com>
2017-10-17 14:36:27 +0200

    Add for loop that did not work - Dafvid

--- src/com/Snakes/Main.java
+++ src/com/Snakes/Main.java
@@ -4,44 +4,71 @@ import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.googlecode.lanterna.input.Key;
 
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Terminal terminal = getTerminal();
-        Player playerOne = new Player();
-        Key key = terminal.readInput();
-        
+        List<Player> players = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            players.add(new Player());
+        }
+//        Player playerOne = new Player();
+//        Player playerTwo = new Player();
+        boolean isAlive = true;
+
         // Menu, deciding how many players/AIs to participate. Level
 
-        // Playerklass avgör längd, kollisionsavkänning och sparar koordinat och riktning
-
         // Count down
-        while(true) {
+        while (isAlive) {
 
             terminal.clearScreen();
             // Put player on terminal
-            terminal.moveCursor(playerOne.getHead().getX(), playerOne.getHead().getY());
-            terminal.putCharacter('O');
-            
+            for (int j = 0; j < players.size(); j++) {
+                for (int i = 0; i < players.get(j).getTail().size(); i++) {
+                    terminal.moveCursor(players.get(j).getTail().get(i).getX(), players.get(j).getTail().get(i).getY());
+                    if (i != 0)
+                        terminal.putCharacter('Z');
+                    else
+                        terminal.putCharacter('♥');
+                }
+            }
+
             // Sleep
-            Thread.sleep(100);
-            
+            Thread.sleep(150);
+
             // Move
-            playerOne.keyListen(key);
-            playerOne.move();
-            
-            
+            Key key = terminal.readInput();
+            for (int i = 0; i < players.size(); i++) {
+                players.get(i).keyListen(key);
+                players.get(i).move();
+            }
 
-            // Movementklass som översätter key till ett riktningsbyte - en per spelare
 
-            // Utskrivningsklass
+            // Check for crash
+            Player.checkForCrash(players);
 
-            // Väntar X länge
         }
+        System.out.println("GAME OVER");
     }
-    
+
+//    private static boolean checkForCrash(List<Player> players, boolean isAlive) {
+//        for (int i = 0; i < players.size(); i++) {
+//            for (int j = 1; j < players.get(i).getTail().size(); j++) {
+//                if (players.get(i).getHead().getX() == players.get(i).getTail().get(j).getX() && players.get(i).getHead().getY() == players.get(i).getTail().get(j).getY()) {
+//                    isAlive = false;
+//                    break;
+//                }
+//            }
+//        }
+//        return isAlive;
+//    }
+
     private static Terminal getTerminal() {
         Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
         terminal.enterPrivateMode();
--- src/com/Snakes/Player.java
+++ src/com/Snakes/Player.java
@@ -14,8 +14,14 @@ public class Player {
     private List<Tail> tail = new LinkedList<>();
     private boolean up, down, right, left;
     private char dir = 'd';
-    final char UP = 'w', DOWN = 's', LEFT = 'a', RIGHT = 'd';
-    
+    private final char UP = 'w', DOWN = 's', LEFT = 'a', RIGHT = 'd';
+    private int countR = 0;
+    private boolean isAlive = true;
+
+    public List<Tail> getTail() {
+        return tail;
+    }
+
     public Player() {
         Random random = new Random();
         
@@ -35,7 +41,6 @@ public class Player {
         } catch (NullPointerException e) {
         }
     }
-    
     
     public void move() {
         // Update direction
@@ -58,6 +63,7 @@ public class Player {
         } else if (left) {
             tail.add(0, new Tail(tail.get(0).getX() - 1, tail.get(0).getY()));
         }
+        removeTail();
     }
     
     private void updateDirection() {
@@ -96,5 +102,44 @@ public class Player {
                 break;
         }
     }
-}
 
+    private void removeTail() {
+        countR++;
+        if (countR % 10 == 0) {
+            return;
+        }
+        tail.remove(tail.size()-1);
+    }
+
+    public static void checkForCrash(List<Player> players) {
+        // For each player
+        for (int i = 0; i < players.size(); i++) {
+            // For all players
+            for (int j = 0; j < players.size(); j++) {
+                // For all snake parts
+                for (int k = 0; k < players.get(j).getTail().size(); k++) {
+                    // Check if comparing tail is own head
+                    if (players.get(i).getHead() == players.get(j).getHead()) {
+                        continue;
+                    }
+                    // Check crash
+                    if (players.get(i).getHead().getX() == players.get(j).getTail().get(k).getX() &&
+                            players.get(i).getHead().getY() == players.get(j).getTail().get(k).getY()) {
+                        players.get(i).isAlive = false;
+                    }
+                }
+            }
+        }
+
+
+//        for (int i = 0; i < players.size(); i++) {
+//            for (int j = 1; j < players.get(i).getTail().size(); j++) {
+//                if (players.get(i).getHead().getX() == players.get(i).getTail().get(j).getX() && players.get(i).getHead().getY() == players.get(i).getTail().get(j).getY()) {
+//                    isAlive = false;
+//                    break;
+//                }
+//            }
+//        }
+//        return isAlive;
+    }
+}
